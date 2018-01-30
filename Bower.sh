72beecf4fd7ceb9ec28acfe20c5fba31ab2d0667
Garali Mohamed Karim <garalimedkarim2@gmail.com>
2018-01-30 08:29:25 +0100

    30/01/2018

---
+++ Bower.sh
@@ -0,0 +1,31 @@
+Bower :
+
+Install:
+	npm install -g bower	
+
+Installing package methods:
+	// installs the project dependencies listed in bower.json
+	bower install
+	// registered package
+	bower install jquery
+	// GitHub shorthand
+	bower install desandro/masonry
+	// Git endpoint
+	bower install git://github.com/user/package.git
+	// URL
+	bower install http://example.com/script.js
+
+
+Start Bower in a project:
+	bower init
+Install a package in the project
+	bower install <package>
+	bower install <package> --save
+
+Bower packages :
+	https://bower.io/search/
+
+	bower list
+	bower list --paths
+	bower uninstall jquery
+	bower install jquery#1.9.2	
---
+++ npm.sh
@@ -0,0 +1,14 @@
+
+search packages:
+	npm install -g npmsearch
+	npmsearch <keywords> [options]
+
+	options:
+		--exact use exact keywords only (bool)
+		--relevance relevance factor for sorting number > 0 default 0.25
+		--downloads downloads factor for sorting number > 0 default 1.5
+		--freshness freshness factor for sorting number > 0 default 0.25
+		--halflife halflife of download count value in days `default 30
+		--aging halflife of package freshness in days default 180
+		--dataAge maximum data age in (days) or fetch from registry (default 1.5)
+		--refresh force data update (bool
